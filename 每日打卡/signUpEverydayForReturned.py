cac0fba67aa9397bad5c1ad7261677d31b4f5079
damaoooo <45312642+damaoooo@users.noreply.github.com>
2021-11-29 18:45:36 +0800

    fix
    你电修改了后端地址，但是这个小程序本质还是没变

--- 每日打卡/signUpEverydayForReturned.py
+++ 每日打卡/signUpEverydayForReturned.py
@@ -1,14 +1,21 @@
 import requests
 import json
+import time
 
 def sign_in():
-    session = {'JSESSIONID':'自己抓'}
+
+    session = {'JSESSIONID':"自己抓", 
+               'SESSION': '自己抓',
+               'Hm_lvt_fe3b7a223fc08c795f0f4b6350703e6f': str(int(time.time())),
+               'Hm_lpvt_fe3b7a223fc08c795f0f4b6350703e6f': str(int(time.time()))
+               }
     url = 'https://jzsz.uestc.edu.cn/wxvacation/monitorRegisterForReturned'
     data = {
-	"healthCondition": "正常",
-	"todayMorningTemperature": "36°C~36.5°C",
-	"yesterdayEveningTemperature": "36°C~36.5°C",
-	"yesterdayMiddayTemperature": "36°C~36.5°C"
+    "healthCondition": "正常",
+    "todayMorningTemperature": "36°C~36.5°C",
+    "yesterdayEveningTemperature": "36°C~36.5°C",
+    "yesterdayMiddayTemperature": "36°C~36.5°C",
+    "location": "四川省成都市郫都区"
     }
     head = {'Content-Type':'application/json', 'charset':'utf-8', 'x-tag': 'flyio'}
     r = requests.post(url, cookies=session, headers=head, data=json.dumps(data))
